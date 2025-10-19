bee98d63afc8037447d4a3870f25017a99c4b038
Josefin Fuchs <jofu0001@stud.ek.dk>
2025-10-19 20:54:28 +0200

    ...

--- menu.js
+++ menu.js
@@ -1,11 +1,9 @@
 const burger = document.querySelector(".burger");
 const overlay = document.querySelector(".overlay-menu");
 const closeBtn = document.querySelector(".close-btn");
-
 burger.addEventListener("click", () => {
   overlay.classList.add("active");
 });
-
 closeBtn.addEventListener("click", () => {
   overlay.classList.remove("active");
 });
